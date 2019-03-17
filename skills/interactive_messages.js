462049b0a8e1249a04326ba15280ed191dc5fc78
Dan Pickett <dan.pickett@launchacademy.com>
2019-03-17 14:02:28 -0400

    use nodemon for easy development

--- skills/interactive_messages.js
+++ skills/interactive_messages.js
@@ -1,5 +1,4 @@
 module.exports = function(controller) {
-
     // create special handlers for certain actions in buttons
     // if the button action is 'say', act as if user said that thing
     controller.middleware.receive.use(function(bot, message, next) {
